28c95456d0785d3a159fd8898637cbd347886b15
Mobina Kadaei <ZxcvbnM@987654321>
2026-05-17 22:31:00 +0200

    add the hosting Netlify URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      Weather App, coded by Mobina Kadaei and is hosted on <a href="https://react-weather-mobina.netlify.app/">Netlify</a>
     </div>
   );
 }
