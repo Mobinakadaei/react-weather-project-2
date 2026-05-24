5abecf173d5eeed9b671a85f4ec0ace32c087af7
Mobina Kadaei <ZxcvbnM@987654321>
2026-05-24 23:27:34 +0200

    add github repository link to the weather app url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App, coded by Mobina Kadaei and is hosted on <a href="https://react-weather-mobina.netlify.app/">Netlify</a>
+      Weather App, coded by <a href="https://github.com/Mobinakadaei/react-weather-project-2">Mobina Kadaei</a> and is hosted on <a href="https://react-weather-mobina.netlify.app/">Netlify</a>
     </div>
   );
 }
