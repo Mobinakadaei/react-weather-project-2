be3dc24cb81cfd410f433b18b5301364a77d0f75
Mobina Kadaei <ZxcvbnM@987654321>
2026-05-27 17:24:59 +0200

    add html and some parts of css, also add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
+      <Weather />
       Weather App, coded by <a href="https://github.com/Mobinakadaei/react-weather-project-2">Mobina Kadaei</a> and is hosted on <a href="https://react-weather-mobina.netlify.app/">Netlify</a>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Weather() {
+    return (<div className="weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+                    <input type="search" placeholder="Insert the name of a city" className="form-control" autoFocus="on" />
+                </div>
+                <div className="col-3">
+                    <input type="submit" value="Search" className="btn btn-primary w-100" />
+                </div>
+
+
+            </div>
+        </form>
+        <h1>Utrecht</h1>
+        <ul>
+            <li>
+                Wednesday 12:00
+            </li>
+            <li>
+                Sunny
+            </li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg" alt="Partly sunny" /> 26°C
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>Precipitation: 1% </li>
+                    <li>Humidity: 47% </li>
+                    <li>Wind: 14 km/h </li>
+                </ul>
+            </div>
+        </div>
+    </div>)
+}
